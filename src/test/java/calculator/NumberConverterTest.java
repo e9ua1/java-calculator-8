093dc849762edebbc2544c6427dcaf28b7018f24
dongheeHan <ark182818@gmail.com>
2025-10-18 19:12:41 +0900

    fix: NumberConverterTest 클래스명 오타 수정

--- src/test/java/calculator/NumberConverterTest.java
+++ src/test/java/calculator/NumberConverterTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class NumberConverterTest {
     @Test
     void 문자열을_정수로_반환한다() {
-        NumberConverter converter = new Converter();
+        NumberConverter converter = new NumberConverter();
         long result = converter.convert("123");
         assertThat(result).isEqualTo(123L);
     }
