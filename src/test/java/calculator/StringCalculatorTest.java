33e836510f6b1bad37f629bb79c2e17373224d90
dongheeHan <ark182818@gmail.com>
2025-10-18 21:43:24 +0900

    test: StringCalculator null 또는 빈 문자열 0 반환 테스트 추가

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StringCalculatorTest {
+
+    @Test
+    void null_또는_빈_문자열_입력시_0을_반환한다() {
+        StringCalculator calculator = new StringCalculator();
+        assertThat(calculator.calculate(null)).isEqualTo(0L);
+        assertThat(calculator.calculate("")).isEqualTo(0L);
+    }
+}
