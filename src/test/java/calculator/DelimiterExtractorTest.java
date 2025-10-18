3aea7e8da71cb2ee25a2dac82557bd3d0a246da7
dongheeHan <ark182818@gmail.com>
2025-10-18 20:57:35 +0900

    test: DelimiterExtractor 커스텀 구분자 존재 여부 확인 테스트 추가

---
+++ src/test/java/calculator/DelimiterExtractorTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DelimiterExtractorTest {
+
+    @Test
+    void 커스텀_구분자가_있는지_확인한다() {
+        DelimiterExtractor extractor = new DelimiterExtractor();
+        assertThat(extractor.hasCustomDelimiter("//;\n1;2")).isTrue();
+        assertThat(extractor.hasCustomDelimiter("1,2")).isFalse();
+    }
+}
