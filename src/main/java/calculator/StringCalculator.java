a73e62afd03ec2132ed26f5f4f7bb06b7a1ebcaf
dongheeHan <ark182818@gmail.com>
2025-10-18 21:51:48 +0900

    feat: StringCalculator 숫자 하나 입력 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,15 +3,22 @@ package calculator;
 public class StringCalculator {
 
     private final InputValidator validator;
+    private final NumberConverter numberConverter;
+    private final Calculator calculator;
 
     public StringCalculator() {
         this.validator = new InputValidator();
+        this.numberConverter = new NumberConverter();
+        this.calculator = new Calculator();
     }
 
     public long calculate(String input) {
         if (!validator.isValid(input) || input.isEmpty()) {
             return 0L;
         }
-        return 0L;
+
+        long number = numberConverter.convert(input);
+        validator.validateNumber(number);
+        return calculator.sum(new long[]{number});
     }
 }
