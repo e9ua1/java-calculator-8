fa4a579a64c2e2e42cb4483b89d1f7a608d6cf7d
dongheeHan <ark182818@gmail.com>
2025-10-18 20:32:59 +0900

    test: StringSplitter 단일 구분자 분리 테스트 추가

---
+++ src/test/java/calculator/StringSplitterTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringSplitterTest {
+
+    @Test
+    void 단일_구분자로_문자열을_분리한다() {
+        StringSplitter splitter = new StringSplitter();
+        String[] result = splitter.split("1,2,3", ",");
+        assertThat(result).containsExactly("1", "2", "3");
+    }
+}
