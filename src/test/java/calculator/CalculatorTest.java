7deeefe8fde16f3efc0b9287d339eaf395bae484
dongheeHan <ark182818@gmail.com>
2025-10-18 18:49:18 +0900

    test: Calculator 빈 배열은 0 반환 테스트 추가

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+class CalculatorTest {
+    @Test
+    void 빈_배열은_0을_반환한다() {
+        Calculator calculator = new Calculator();
+        long result = calculator.sum(new long[]{});
+        assertThat(result).isEqualsTo(0L);
+    }
+}
