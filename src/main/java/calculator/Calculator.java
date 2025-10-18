5c2196a7fcb0eca1c255aad2cb3afa9adbd0397b
dongheeHan <ark182818@gmail.com>
2025-10-18 18:52:16 +0900

    feat: Calculator 빈 배열은 0 반환 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+
+    public long sum(long[] numbers) {
+        if (numbers.length == 0) {
+            return 0L;
+        }
+        return 0L;
+    }
+}
