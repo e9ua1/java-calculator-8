ca159f99349cb152e40779db5a2db116557cb745
dongheeHan <ark182818@gmail.com>
2025-10-18 19:15:30 +0900

    feat: NumberConverter 공백 제거 후 변환 구현

--- src/main/java/calculator/NumberConverter.java
+++ src/main/java/calculator/NumberConverter.java
@@ -3,6 +3,6 @@ package calculator;
 public class NumberConverter {
 
     public long convert(String numberStr) {
-        return Long.parseLong(numberStr);
+        return Long.parseLong(numberStr.trim());
     }
 }
