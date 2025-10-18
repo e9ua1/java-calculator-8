5d7e7f533ecff7257f1f6e1d0868020e2768be41
dongheeHan <ark182818@gmail.com>
2025-10-18 23:13:14 +0900

    feat: DelimiterExtractor 이스케이프된 개행 문자 처리 구현

--- src/main/java/calculator/DelimiterExtractor.java
+++ src/main/java/calculator/DelimiterExtractor.java
@@ -7,12 +7,28 @@ public class DelimiterExtractor {
     }
 
     public String extractCustomDelimiter(String input) {
-        int delimiterEndIndex = input.indexOf("\n");
+        int delimiterEndIndex = findDelimiterEndIndex(input);
         return input.substring(2, delimiterEndIndex);
     }
 
     public String extractNumbers(String input) {
-        int delimiterEndIndex = input.indexOf("\n");
-        return input.substring(delimiterEndIndex + 1);
+        int delimiterEndIndex = findDelimiterEndIndex(input);
+        int newlineLength = getNewlineLength(input, delimiterEndIndex);
+        return input.substring(delimiterEndIndex + newlineLength);
+    }
+
+    private int findDelimiterEndIndex(String input) {
+        int realNewline = input.indexOf("\n");
+        if (realNewline != -1) {
+            return realNewline;
+        }
+        return input.indexOf("\\n");
+    }
+
+    private int getNewlineLength(String input, int index) {
+        if (index + 1 < input.length() && input.charAt(index) == '\\') {
+            return 2;
+        }
+        return 1;
     }
 }
