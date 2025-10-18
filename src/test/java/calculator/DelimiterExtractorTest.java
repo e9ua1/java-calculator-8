d3ce748cc1ef3e168340046088530c07e5f86168
dongheeHan <ark182818@gmail.com>
2025-10-18 23:01:46 +0900

    test: DelimiterExtractor 이스케이프된 개행 문자 처리 테스트 추가

--- src/test/java/calculator/DelimiterExtractorTest.java
+++ src/test/java/calculator/DelimiterExtractorTest.java
@@ -25,4 +25,17 @@ public class DelimiterExtractorTest {
         DelimiterExtractor extractor = new DelimiterExtractor();
         assertThat(extractor.extractNumbers("//;\n1;2;3")).isEqualTo("1;2;3");
     }
+
+    @Test
+    void 이스케이프된_개행문자로_커스텀_구분자를_추출한다() {
+        DelimiterExtractor extractor = new DelimiterExtractor();
+        assertThat(extractor.extractCustomDelimiter("//;\\n1;2")).isEqualTo(";");
+        assertThat(extractor.extractCustomDelimiter("//|\\n1|2")).isEqualTo("|");
+    }
+
+    @Test
+    void 이스케이프된_개행문자로_숫자_부분을_추출한다() {
+        DelimiterExtractor extractor = new DelimiterExtractor();
+        assertThat(extractor.extractNumbers("//;\\n1;2;3")).isEqualTo("1;2;3");
+    }
 }
