75f41a443676f8309da61652d01cc8a1067053ac
dongheeHan <ark182818@gmail.com>
2025-10-18 20:43:10 +0900

    feat: StringSplitter 기본 구분자 분리 구현

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -2,7 +2,13 @@ package calculator;
 
 public class StringSplitter {
 
+    private static final String DEFAULT_DELIMITERS = ",|:";
+
     public String[] split(String input, String delimiter) {
         return input.split(delimiter);
+    }
+
+    public String[] splitByDefaultDelimiters(String input) {
+        return input.split(DEFAULT_DELIMITERS);
     }
 }
