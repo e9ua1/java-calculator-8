b73e95f7faaa76a9400c2d86bdefc6a385f23628
dongheeHan <ark182818@gmail.com>
2025-10-18 19:50:37 +0900

    feat: InputValidator 음수 검증 구현

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -5,4 +5,10 @@ public class InputValidator {
     public boolean isValid(String input) {
         return input != null;
     }
+
+    public void validateNumber(long number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("음수는 허용되지 않습니다: " + number);
+        }
+    }
 }
