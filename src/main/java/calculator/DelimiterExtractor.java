469ca74fb97dcbaaa2f884f20569157a1dc80d2e
dongheeHan <ark182818@gmail.com>
2025-10-18 21:29:12 +0900

    feat: DelimiterExtractor 커스텀 구분자 추출 구현

--- src/main/java/calculator/DelimiterExtractor.java
+++ src/main/java/calculator/DelimiterExtractor.java
@@ -5,4 +5,9 @@ public class DelimiterExtractor {
     public boolean hasCustomDelimiter(String input) {
         return input.startsWith("//");
     }
+
+    public String extractCustomDelimiter(String input) {
+        int delimiterEndIndex = input.indexOf("\n");
+        return input.substring(2, delimiterEndIndex);
+    }
 }
