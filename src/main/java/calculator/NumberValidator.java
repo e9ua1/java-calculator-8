d57e277d6f988700758c9f61edbc64e6a28a78e2
dongheeHan <ark182818@gmail.com>
2025-10-19 16:14:22 +0900

    refactor: InputValidator를 NumberValidator로 이름 변경
    - 숫자 검증 책임에 맞게 이름 변경
    - SRP 원칙에 따라 역할 명확화
    - isValid 메서드 제거하고 validate로 통일

--- src/main/java/calculator/NumberValidator.java
+++ src/main/java/calculator/NumberValidator.java
@@ -2,13 +2,9 @@ package calculator;
 
 import java.math.BigInteger;
 
-public class InputValidator {
+public class NumberValidator {
 
-    public boolean isValid(String input) {
-        return input != null;
-    }
-
-    public void validateNumber(BigInteger number) {
+    public void validate(BigInteger number) {
         if (number.compareTo(BigInteger.ZERO) < 0) {
             throw new IllegalArgumentException("음수는 허용되지 않습니다: " + number);
         }
--- src/test/java/calculator/NumberValidatorTest.java
+++ src/test/java/calculator/NumberValidatorTest.java
@@ -1,31 +1,17 @@
 package calculator;
 
 import java.math.BigInteger;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-public class InputValidatorTest {
-
-    @Test
-    void null_입력시_false를_반환한다() {
-        NumberValidator validator = new NumberValidator();
-        boolean result = validator.isValid(null);
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    void 빈_문자열은_유효하다() {
-        NumberValidator validator = new NumberValidator();
-        boolean result = validator.isValid("");
-        assertThat(result).isTrue();
-    }
+public class NumberValidatorTest {
 
     @Test
     void 음수_입력시_예외를_발생시킨다() {
         NumberValidator validator = new NumberValidator();
-        assertThatThrownBy(() -> validator.validateNumber(BigInteger.valueOf(-1)))
+        assertThatThrownBy(() -> validator.validate(BigInteger.valueOf(-1)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("음수는 허용되지 않습니다");
     }
@@ -33,7 +19,8 @@ public class InputValidatorTest {
     @Test
     void 양수는_검증을_통과한다() {
         NumberValidator validator = new NumberValidator();
-        validator.validateNumber(BigInteger.valueOf(1));
-        validator.validateNumber(new BigInteger("999999999999999999"));
+        validator.validate(BigInteger.ZERO);
+        validator.validate(BigInteger.valueOf(1));
+        validator.validate(new BigInteger("999999999999999999"));
     }
 }
