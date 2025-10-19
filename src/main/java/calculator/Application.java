0746cc4f00961ab699b5f009eebe12c198af5eaa
dongheeHan <ark182818@gmail.com>
2025-10-19 16:16:39 +0900

    refactor: Application에 객체 생성 팩토리 메서드 추가
    - createCalculator() 메서드로 의존성 조립
    - 객체 생성 책임 분리로 SRP 준수
    - 가독성 및 유지보수성 향상

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,17 +1,40 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.math.BigInteger;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import calculator.parser.CustomDelimiterParser;
+import calculator.parser.DefaultDelimiterParser;
+import calculator.parser.DelimiterParser;
+import calculator.parser.SingleNumberParser;
 
 public class Application {
 
     public static void main(String[] args) {
+        StringCalculator calculator = createCalculator();
+
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        StringCalculator calculator = new StringCalculator();
         BigInteger result = calculator.calculate(input);
-
         System.out.println("결과 : " + result);
+    }
+
+    private static StringCalculator createCalculator() {
+        DelimiterExtractor extractor = new DelimiterExtractor();
+        StringSplitter splitter = new StringSplitter();
+        NumberConverter converter = new NumberConverter();
+        NumberValidator validator = new NumberValidator();
+        Adder adder = new Adder();
+
+        List<DelimiterParser> parsers = List.of(
+                new CustomDelimiterParser(extractor, splitter),
+                new DefaultDelimiterParser(splitter),
+                new SingleNumberParser()
+        );
+
+        return new StringCalculator(parsers, converter, validator, adder);
     }
 }
