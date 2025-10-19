a7803b2b34ee0617e7636f19bf4e407edc97dd10
dongheeHan <ark182818@gmail.com>
2025-10-19 16:27:35 +0900

    style: import 순서를 Java 코드 컨벤션에 맞게 정리
    - Java 표준 라이브러리 (java.*, javax.*)
    - 외부 라이브러리 (camp.nextstep.*)
    - 프로젝트 내부 클래스 (calculator.*)
    - static import (제일 마지막)
    - 각 그룹 사이 빈 줄로 구분하여 가독성 향상

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 
 class ApplicationTest extends NsTest {
 
--- src/test/java/calculator/NumberConverterTest.java
+++ src/test/java/calculator/NumberConverterTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.math.BigInteger;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
